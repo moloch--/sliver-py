2319d0d303d88f36290b089762530549a2ca8081
moloch-- <875022+moloch--@users.noreply.github.com>
2021-04-11 17:04:09 -0500

    Resolve src dir regardless of cwd

--- docs/conf.py
+++ docs/conf.py
@@ -11,7 +11,9 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../src'))
+
+DOCS = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.abspath(os.path.join(DOCS, '..', 'src')))
 
 
 
