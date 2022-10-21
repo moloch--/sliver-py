afab651b5623386c7980fb662f9570d7fe5ce720
Leron Gray <daddycocoaman@gmail.com>
2022-10-21 15:21:04 -0700

    add typing extensions for 3.7

--- src/sliver/_protocols.py
+++ src/sliver/_protocols.py
@@ -1,10 +1,13 @@
-from typing import Protocol
-
 from .pb.commonpb.common_pb2 import Request
 from .pb.rpcpb.services_pb2_grpc import SliverRPCStub
 
+try:
+    from typing import Protocol
+except ImportError:
+    from typing_extensions import Protocol
 
-class PbWithRequestProp(Protocol):
+
+class PbWithRequestProp(Protocol):  # type: ignore
     """Protocol for protobuf with Request field"""
 
     @property
@@ -12,7 +15,7 @@ class PbWithRequestProp(Protocol):
         ...
 
 
-class InteractiveObject(Protocol):
+class InteractiveObject(Protocol):  # type: ignore
     """Protocol for objects with interactive methods"""
 
     @property
